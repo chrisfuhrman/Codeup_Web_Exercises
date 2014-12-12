0c62e5ad68ad0c4c7a8d3bedd83d3cbc23c7ee29
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-12-12 13:58:15 -0600

    js for html lesson

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,24 @@
-console.log('Hello from external JavaScript');
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+switch (color) {
+	case 'red':
+		var say = 'Red is awesome';
+		break;
+	case 'orange':
+		var say = 'Orange is awesome';
+		break;
+	case 'yellow':
+		var say = 'Yellow is awesome';
+		break;
+	case 'green':
+		var say = 'Green is awesome';
+		break;
+	case 'blue':
+		var say = 'Blue is awesome';
+		break;
+	default:
+		var say = 'I do not know anything by that color.';
+	}
+
+	console.log(say);
