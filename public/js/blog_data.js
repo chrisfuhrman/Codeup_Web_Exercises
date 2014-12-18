ae4de0b47c6e5ffaaeb6e193dd6f4e3cb803a6f8
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-12-18 16:02:58 -0600

    JSON Blog

---
+++ public/js/blog_data.js
@@ -0,0 +1,29 @@
+var data = [
+	{
+	"title": "Blog Title 1",
+	"date": "2014-12-14",
+	"author": "Chris Fuhrman",
+	"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, reiciendis, aliquam. Ad itaque obcaecati error et. Sapiente labore molestias omnis dolores, totam fugit ex necessitatibus, commodi nihil soluta quaerat impedit."
+	}, 
+
+	{
+	"title": "Blog Title 2",
+	"date": "2014-12-15",
+	"author": "Chris Fuhrman",
+	"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, reiciendis,  ex necessitatibus, commodi nihil soluta quaerat impedit."
+	},
+
+	{
+	"title": "Blog Title 3",
+	"date": "2014-12-16",
+	"author": "Chris Fuhrman",
+	"content": "Lorem ipsum dolor sit amet HHIHIHIHIHIHIHIHIHIHIHIHI, consectetur adipisicing elit. Cumque, reiciendis,  ex necessitatibus, commodi nihil soluta quaerat impedit."
+	},
+
+	{
+	"title": "Blog Title 4",
+	"date": "2014-12-17",
+	"author": "Chris Fuhrman",
+	"content": "Lorem ipsum dolor tibus, commodi nihil soluta quaerat impedit."
+	}
+];
